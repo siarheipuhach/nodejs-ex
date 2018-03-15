dedd4bbdeb3d8f6eb14cc1700967b4c333bbb106
Siarhei Puhach <siarhei.puhach@itechart-group.com>
2018-03-15 16:07:20 +0300

    Changed server file

---
+++ routes/users.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+// Register 
+var User = require('../models/user');
+
+
+
+router.post('/register', (req, res) => {
+    var name = req.body.name;
+    var email = req.body.email;
+    var password = req.body.password;   
+    
+    if (name && email && password){
+        var newUser = new User({
+            name: name,
+            email: email,
+            password: password
+        });
+        User.createUser(newUser, function(err, user){
+            if(err) throw err;
+            console.log(user);
+        });
+        return res.send({'Success': 'You are registered'})
+    }
+    return res.send('Error')
+  });
+
+passport.use(new LocalStrategy(
+    {usernameField: "email", passwordField:"password"},
+    function(email, password, done) {
+        User.getUserByEmail(email, function(err, user){
+            if(err) throw err;
+            if(!user){
+                return done(null, false, {'message' :'Unknown'})
+            }
+            User.comparePassword(password, user.password, function(err, isMatch){
+                if (err) throw err;
+                if (isMatch){
+                    return done(null, user);
+                } else {
+                    return done(null, false, {'message': 'Invalid password'})
+                }
+            });
+        });
+
+    }
+));
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+    User.getUserById(id, function(err, user){
+        done(err, user)
+    });
+});
+
+// Login
+router.post('/login',
+  passport.authenticate('local', { successFlash: 'Welcome!', failureFlash: 'Invalid username or password.'}),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.send('you were logged in');
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,108 +1,76 @@
-//  OpenShift sample Node application
 var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
+    morgan  = require('morgan'),
+    path = require('path'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    exphbs = require('express-handlebars'),
+    expressValidator = require('express-validator'),
+    flash = require('connect-flash'),
+    session = require('express-session'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    mongo = require('mongodb'),
+    mongoose = require('mongoose');
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var mongoURL = 'mongodb://localhost:27017/financer';
+   
+mongoose.connect(mongoURL);
+var db = mongoose.connection;
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+var app = express();
 
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
+//View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'layout',
+    layoutsDir: path.join(__dirname, '..', 'views', 'layouts'),
+    partialsDir: path.join(__dirname, '..', 'views', 'partials')}
+));
+app.set('view engine', 'handlebars');
 
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+//BodyParser Middleware
 
-  }
-}
-var db = null,
-    dbDetails = new Object();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 
-var initDb = function(callback) {
-  if (mongoURL == null) return;
+// Set Static Folder
+app.use(express.static(path.join(__dirname,'public')));
 
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
+// Express Session
+app.use(session({
+    secret:'secret',
+    saveUninitialized: true,
+    resave: true
+}))
 
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
+//Pasport init
+app.use(passport.initialize());
+app.use(passport.session());
 
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
-
-    console.log('Connected to MongoDB at: %s', mongoURL);
-  });
-};
-
-app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      if (err) {
-        console.log('Error running count. Message:\n'+err);
-      }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
+app.use(flash());
+app.use(function(req, res, next){
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error')
+    next();
 });
 
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCountChange1: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCountIdle: -1 }');
-  }
+app.use('/', routes);
+app.use('/users', users)
+
+// Set port
+app.set('port', (process.env.PORT || 3000));
+app.listen(app.get('port'), function(){
+    console.log('Server is running on port '+ app.get('port'));
 });
+// app.use(expressSession({secret: 'mySecretKey'}));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
 
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
+// mongoose.connect(mongoURL);
 
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
-
-module.exports = app ;
+// module.exports = app;
