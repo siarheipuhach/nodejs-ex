e3878f4b0b9ae80842b103bbeca2c8b16812aae2
Siarhei Puhach <siarhei.puhach@itechart-group.com>
2018-05-08 10:41:42 +0300

    Upload path changed

--- routes/items.js
+++ routes/items.js
@@ -6,7 +6,7 @@ var Item = require('../models/item');
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb){
-    cb(null, '../uploads/');
+    cb(null, __dirname + '../uploads/');
   },
   filename: function(req, file, cb){
     cb(null, new Date().toISOString()+file.originalname);
